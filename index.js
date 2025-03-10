39ac1e373cc5b2f9a162db4ab375943a233bae83
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-03-10 18:46:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://netfree.cc/mobile/home';
+const nggUrl = 'https://tiktok.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'netfree.cc') {
+    if (req.headers.host === 'tiktok.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
