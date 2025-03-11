5b540fe26a61b057172bdbca50e88acd2095d8e9
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-03-10 21:51:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://now.gg/apps/a/19900/b.html';
+const nggUrl = 'https://worldguessr.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'now.gg') {
+    if (req.headers.host === 'worldguessr.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
