15d4ffb56e2fa6c367ad730e42b2b94214209f59
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-03-10 20:28:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://thundr-unblocker.vercel.app/service/hvtrs8%2F-wuw%2Ctkkvoi.aoo%2Fgxrlmrg';
+const nggUrl = 'https://now.gg/apps/a/19900/b.html';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'deadshot.io') {
+    if (req.headers.host === 'now.gg') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
