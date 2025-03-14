cc005bf937aa0c367f27aef8fac29ee70f66d5f8
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-03-13 21:38:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://thefiremovies.vercel.app/';
+const nggUrl = 'https://deadsimplechat.com/NFlUsmyDQ/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
