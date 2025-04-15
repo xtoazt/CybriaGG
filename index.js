5529bb004e25a42ceb5fbfcdf30601f9d2e71940
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-04-14 21:48:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://poki.com';
+const nggUrl = 'https://onlycircle.up.railway.app/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'poki.com') {
+    if (req.headers.host === 'onlycircle.up.railway.app') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
